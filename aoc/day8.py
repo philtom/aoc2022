95cb0ed360ed1ea54f822e0503c1297007f85b80
Phil Tom <philtom@gmail.com>
2022-12-14 01:25:47 -0600

    day 8 part 2 done

--- aoc/day8.py
+++ aoc/day8.py
@@ -59,6 +59,35 @@ def count_visible(grid: list[list[int]]) -> int:
     return sum(1 for visibile in visibilities if visibile)
 
 
+
+def calc_scenic_score(grid: list[list[int]], row: int, col: int) -> int:
+    row_count = len(grid)
+    col_count = len(grid[0])
+    height = grid[row][col]
+    def calc_view_distance(coordinates: Iterator[tuple[int, int]]) -> int:
+        score = 0
+        for coordinate in coordinates:
+            score += 1
+            if grid[coordinate[0]][coordinate[1]] >= height:
+                break
+        return score
+
+    return calc_view_distance(
+            itertools.product(reversed(range(0, row)), [col])
+        ) * calc_view_distance(
+            itertools.product(range(row + 1, row_count), [col])
+        ) * calc_view_distance(
+            itertools.product([row], reversed(range(0, col)))
+        ) * calc_view_distance(
+            itertools.product([row], range(col + 1, col_count))
+        ) 
+
+def find_max_scenic_score(grid: list[list[int]]) -> int:
+    coordinates = itertools.product(range(0, len(grid)), range(0, len(grid[0])))
+    scores = map(lambda rc: calc_scenic_score(grid, rc[0], rc[1]), coordinates)
+    return max(scores)
+
+
 def test1():
     input = utils.read_input("input/day8-part1-test.txt")
     grid = create_grid(input)
@@ -71,6 +100,20 @@ def part1():
     print(num_visible)
     assert num_visible == 1705
 
+def test2():
+    input = utils.read_input("input/day8-part1-test.txt")
+    grid = create_grid(input)
+    assert find_max_scenic_score(grid) == 8
+
+def part2():
+    input = utils.read_input("input/day8-part1.txt")
+    grid = create_grid(input)
+    score = find_max_scenic_score(grid)
+    print(score)
+    assert score == 371200
+
 def main():
     test1()
     part1()
+    test2()
+    part2()
