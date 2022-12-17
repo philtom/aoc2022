0a654ffddf71747beca7072f3165dd36e338344f
Phil Tom <philtom@gmail.com>
2022-12-16 23:12:35 -0600

    refactoring

--- aoc/day9.py
+++ aoc/day9.py
@@ -24,7 +24,9 @@ def parse_move(input: str) -> Move:
 
 def process_input(state: State, input: str) -> State:
     move = parse_move(input)
-    return reduce(process_move_increment, itertools.repeat(move.direction, move.distance), state)
+    return reduce(
+        process_move_increment, itertools.repeat(move.direction, move.distance), state
+    )
 
 
 def process_move_increment(state: State, direction: str) -> State:
@@ -33,21 +35,13 @@ def process_move_increment(state: State, direction: str) -> State:
 
 def move_head(state: State, direction: str) -> State:
     if direction == "R":
-        return dataclasses.replace(
-            state, head_coord=move_right(state.head_coord)
-        )
+        return dataclasses.replace(state, head_coord=move_right(state.head_coord))
     elif direction == "L":
-        return dataclasses.replace(
-            state, head_coord=move_left(state.head_coord)
-        )
+        return dataclasses.replace(state, head_coord=move_left(state.head_coord))
     elif direction == "U":
-        return dataclasses.replace(
-            state, head_coord=move_up(state.head_coord)
-        )
+        return dataclasses.replace(state, head_coord=move_up(state.head_coord))
     elif direction == "D":
-        return dataclasses.replace(
-            state, head_coord=move_down(state.head_coord)
-        )
+        return dataclasses.replace(state, head_coord=move_down(state.head_coord))
     else:
         raise ValueError(f"Unknown direction: {direction}")
 
@@ -58,79 +52,75 @@ def move_tail(state: State) -> State:
     H . . . H
     H . T . H
     H . . . H
-      H H H  
+      H H H
     """
     delta_x = state.head_coord[0] - state.tail_coord[0]
     delta_y = state.head_coord[1] - state.tail_coord[1]
     if delta_x == 0 and delta_y == 0:
         return state
 
-    if delta_x == 0:
-        if delta_y > 1:
-            return update_tail_coord(state, move_up(state.tail_coord))
-        elif delta_y < -1:
-            return update_tail_coord(state, move_down(state.tail_coord))
-    elif delta_y == 0:
-        if delta_x > 1:
-            return update_tail_coord(state, move_right(state.tail_coord))
-        elif delta_x < -1:
-            return update_tail_coord(state, move_left(state.tail_coord))
-    elif delta_x > 1:
-        if delta_y > 0:
-            return update_tail_coord(state, move_up(move_right(state.tail_coord)))
-        elif delta_y < 0:
-            return update_tail_coord(state, move_down(move_right(state.tail_coord)))
-    elif delta_x < -1:
-        if delta_y > 0:
-            return update_tail_coord(state, move_up(move_left(state.tail_coord)))
-        elif delta_y < 0:
-            return update_tail_coord(state, move_down(move_left(state.tail_coord)))
-    elif delta_y > 1:
-        if delta_x > 0:
-            return update_tail_coord(state, move_up(move_right(state.tail_coord)))
-        elif delta_x < 0:
-            return update_tail_coord(state, move_up(move_left(state.tail_coord)))
-    elif delta_y < -1:
-        if delta_x > 0:
-            return update_tail_coord(state, move_down(move_right(state.tail_coord)))
-        elif delta_x < 0:
-            return update_tail_coord(state, move_down(move_left(state.tail_coord)))
+    new_coords = state.tail_coord
+    if delta_y == 2 or (delta_y == 1 and abs(delta_x) == 2):
+        # move up
+        new_coords = move_up(new_coords)
 
-    return state
- 
+    if delta_y == -2 or (delta_y == -1 and abs(delta_x) == 2):
+        # move down
+        new_coords = move_down(new_coords)
+
+    if delta_x == 2 or (delta_x == 1 and abs(delta_y) == 2):
+        # move right
+        new_coords = move_right(new_coords)
+
+    if delta_x == -2 or (delta_x == -1 and abs(delta_y) == 2):
+        # move left
+        new_coords = move_left(new_coords)
+
+    return update_tail_coord(state, new_coords)
+
+
 def move_up(coord: tuple[int, int]) -> tuple[int, int]:
-    return (coord[0], coord[1]+1)
-    
+    return (coord[0], coord[1] + 1)
+
+
 def move_down(coord: tuple[int, int]) -> tuple[int, int]:
-    return (coord[0], coord[1]-1)
-    
+    return (coord[0], coord[1] - 1)
+
+
 def move_right(coord: tuple[int, int]) -> tuple[int, int]:
-    return (coord[0]+1, coord[1])
-    
+    return (coord[0] + 1, coord[1])
+
+
 def move_left(coord: tuple[int, int]) -> tuple[int, int]:
-    return (coord[0]-1, coord[1])
-    
+    return (coord[0] - 1, coord[1])
+
+
 def update_tail_coord(state: State, tail_coord: tuple[int, int]) -> State:
-    return dataclasses.replace(state, tail_coord=tail_coord, tail_history=state.tail_history | {tail_coord})
+    return dataclasses.replace(
+        state, tail_coord=tail_coord, tail_history=state.tail_history | {tail_coord}
+    )
+
 
 def test1():
     inputs = utils.read_input("input/day9-part1-test.txt")
     state = reduce(
         process_input,
         inputs,
-        State(head_coord=(0, 0), tail_coord=(0, 0), tail_history={(0,0)}),
+        State(head_coord=(0, 0), tail_coord=(0, 0), tail_history={(0, 0)}),
     )
     assert len(state.tail_history) == 13
+
 
 def part1():
     inputs = utils.read_input("input/day9-part1.txt")
     state = reduce(
         process_input,
         inputs,
-        State(head_coord=(0, 0), tail_coord=(0, 0), tail_history={(0,0)}),
+        State(head_coord=(0, 0), tail_coord=(0, 0), tail_history={(0, 0)}),
     )
     print(len(state.tail_history))
     assert len(state.tail_history) == 6367
+
 
 def main():
     test1()
